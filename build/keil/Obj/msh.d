4758f67440ab77164a35399fc070e247d1a640e7
linzibo <202200201046@stumail.sztu.edu.cn>
2025-06-04 19:41:34 +0800

    123

---
+++ build/keil/Obj/msh.d
@@ -0,0 +1,15 @@
+.\build\keil\obj\msh.o: rt-thread\components\finsh\msh.c
+.\build\keil\obj\msh.o: rt-thread\include\rtthread.h
+.\build\keil\obj\msh.o: .\rtconfig.h
+.\build\keil\obj\msh.o: rt-thread\include\rtdebug.h
+.\build\keil\obj\msh.o: rt-thread\include\rtdef.h
+.\build\keil\obj\msh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\build\keil\obj\msh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
+.\build\keil\obj\msh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
+.\build\keil\obj\msh.o: rt-thread\include\rtservice.h
+.\build\keil\obj\msh.o: rt-thread\include\rtm.h
+.\build\keil\obj\msh.o: rt-thread\include\rtthread.h
+.\build\keil\obj\msh.o: rt-thread\components\finsh\finsh.h
+.\build\keil\obj\msh.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
+.\build\keil\obj\msh.o: rt-thread\components\finsh\msh.h
+.\build\keil\obj\msh.o: rt-thread\components\finsh\shell.h
---
+++ rt-thread/components/libc/posix/ipc/system-v/sys/ipc.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-12-07     Meco Man     First version
+ */
+
+#ifndef __SYS_IPC_H__
+#define __SYS_IPC_H__
+
+#include <ipc.c>
+
+
+#endif
